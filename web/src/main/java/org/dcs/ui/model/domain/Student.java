b40d7095182f0a959b90e948035ddb3722c98ab1
tesfayet11 <nospam@gmail.com>
2016-09-06 20:04:21 -0500

    hibernate is creating tables and is working

---
+++ web/src/main/java/org/dcs/ui/model/domain/Student.java
@@ -0,0 +1,36 @@
+package org.dcs.ui.model.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue
+	private Long studentId;
+	private String firstName;
+	private String lastName;
+	public Long getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+
+}
